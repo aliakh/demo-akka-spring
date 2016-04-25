aed3aa0b00b5ff00d3c3155bf3bf39fd736dd323
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2016-04-25 14:07:52 +0400

    Add part 1

---
+++ demo-java-akka-spring/src/main/java/demo/Runner.java
@@ -0,0 +1,50 @@
+package demo;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
+import demo.actor.WorkerActor;
+import demo.di.SpringExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+class Runner implements CommandLineRunner {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private ActorSystem actorSystem;
+
+    @Autowired
+    private SpringExtension springExtension;
+
+    @Override
+    public void run(String[] args) throws Exception {
+        ActorRef workerActor = actorSystem.actorOf(springExtension.props("workerActor"), "worker-actor");
+
+        workerActor.tell(new WorkerActor.Request(), null);
+        workerActor.tell(new WorkerActor.Request(), null);
+        workerActor.tell(new WorkerActor.Request(), null);
+
+        FiniteDuration duration = FiniteDuration.create(1, TimeUnit.SECONDS);
+        Future<Object> awaitable = Patterns.ask(workerActor, new WorkerActor.Response(), Timeout.durationToTimeout(duration));
+
+        try {
+            logger.info("Response: " + Await.result(awaitable, duration));
+        } finally {
+            actorSystem.terminate();
+            Await.result(actorSystem.whenTerminated(), Duration.Inf());
+        }
+    }
+}
